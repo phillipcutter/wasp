054ad24b0d8d58d2a86df798045131bfc022283a
Matija Sosic <matija.sosic@gmail.com>
2023-04-19 12:59:00 +0200

    made blog list page nicer.

--- web/src/theme/BlogPostItems/index.js
+++ web/src/theme/BlogPostItems/index.js
@@ -8,51 +8,54 @@ export default function BlogPostItems({
   component: BlogPostItemComponent = BlogPostItem,
 }) {
   return (
-    <div className='grid grid-cols-1 xl:grid-cols-3 gap-5 mb-8'>
+    <div className='grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-5 mb-8'>
       {items.map((item) => {
-        
-        console.log(item)
         const BlogPostContent = item.content
 
+        console.log(item)
+
         return (
-          <div
+          <article
             className={`
-              rounded-lg shadow-lg
-              border border-neutral-50/20
+              relative
+              rounded-2xl
+
+              bg-[--custom-blog-card-background-color]
+              transition-all
+              shadow-sm shadow-yellow-500/25
+              hover:shadow-lg hover:shadow-yellow-500/25
+
+              border border-neutral-200/20
               flex flex-col space-y-3
+              p-8
             `}
           >
-            <Link to={item.content.metadata.permalink}>
-              <div
+            {/* Image */}
+            <img 
+              src={item.content.frontMatter.image}
                 className={`
-                  h-64 w-full relative overflow-auto overflow-y-hidden
-                  flex justify-center xl:items-center
-                  bg-yellow-500/25
-                  rounded-t-lg
+                  object-cover
+                  rounded-xl mb-6
+                  md:max-w-none h-64 md:-mx-6 md:-mt-6
                 `}
-              >
-                <img 
-                  src={item.content.frontMatter.image}
-                  className='object-cover'
-                />
-              </div>
-            </Link>
+            />
 
-            <div className='p-5'>
-              <Link to={item.content.metadata.permalink}>
-                <h3 className='text-xl h-28'>
-                  {item.content.frontMatter.title}
-                </h3>
-              </Link>
+            {/* Title and other text */}
+            <div className=''>
+              <h3 className='text-xl h-28'>
+                {item.content.frontMatter.title}
+              </h3>
 
               <div className='text-sm text-[--custom-blog-card-timestamp-color]'>
                 By {item.content.metadata.authors[0].name} on {item.content.metadata.formattedDate}
               </div>
             </div>
-          </div>
+
+            <Link to={item.content.metadata.permalink} className='opacity-0 absolute inset-0'/>
+          </article>
         )
 
-        /*
+        /* This is what it looked like right after swizzle --eject.
         return (
           <BlogPostProvider
             key={BlogPostContent.metadata.permalink}
